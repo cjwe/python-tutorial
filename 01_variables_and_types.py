3626cdcf0e28fd2a8b0e1f3f855b9762caed2148
cjwe <christajwest@gmail.com>
2025-03-30 14:25:12 +0200

    Completed first project

--- 01_variables_and_types.py
+++ 01_variables_and_types.py
@@ -8,7 +8,21 @@ is_student = True  # boolean
 print(f"{name} is {age} years old and {height}m tall.")
 print("Is student?", is_student)
 
+print()
+
 # Practice:
 # 1. Create a variable for your favorite color
 # 2. Create a variable for your current year in school or work level
 # 3. Print them out nicely
+
+
+# 1.
+color = "pink"
+
+# 2.
+work_years = 1
+
+# 3.
+print(f"I have been working as a project manager for {work_years} year. My favorite color is {color}.")
+
+print()
