bba8041fbb85157b6ee480f6a93c99bc60448d38
cjwe <christajwest@gmail.com>
2025-04-02 18:13:10 +0200

    Completed #1 in module 3

--- modules/03_functions.py
+++ modules/03_functions.py
@@ -14,3 +14,39 @@ print("3 * 4 =", multiply(3, 4))
 # 1. Write a function that returns the square of a number
 # 2. Write a function that takes a list and returns how many items it has
 # 3. BONUS: Write a function that returns the last item in a list
+
+print()
+print()
+print()
+
+# 1.
+def text(x):
+    return f"The square of {x} is"
+
+def square(x):
+    return x * x
+
+# Method 1
+print(text(4), square(4), ".")
+print()
+
+# Method 2
+print("The square of 4 is", square(4), ".")
+print()
+
+# Method 3
+print(f"The square of 4 is {square(4)}.")
+
+print()
+print()
+print()
+
+# 2.
+
+pastas = ["macaroni","bowtie","spaghetti"]
+
+def eatPasta(pasta):
+    return f"{pasta}"    
+
+print(eatPasta(pastas))
+
