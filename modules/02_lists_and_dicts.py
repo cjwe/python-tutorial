b3cfbaece72fadac5c2c93368b5a846f6dbb3709
cjwe <christajwest@gmail.com>
2025-04-02 17:51:20 +0200

    Completed module 2

--- modules/02_lists_and_dicts.py
+++ modules/02_lists_and_dicts.py
@@ -21,3 +21,24 @@ print("Updated person:", person)
 # 1. Create a list of 3 favorite movies
 # 2. Create a dictionary for a car (brand, model, year)
 # 3. Print a sentence like: "My car is a 2020 Toyota Corolla"
+
+
+print()
+print()
+print()
+print()
+favmovies = ["You've Got Mail", "Pride & Prejudice"]
+print("Favorite movie numero uno:", favmovies[0])
+print()
+favmovies.append("Parasite")
+print("Favorite movies new and improved:", favmovies)
+print()
+
+car = {
+    "brand": "Renault",
+    "model": "Zoe",
+    "year": 2021
+}
+print()
+print(f"My favorite car is a {car["year"]} {car["brand"]} {car["model"]}.") 
+print()
