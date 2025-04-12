0c05bbe5fa086da60b6604714ecbfcef5e56a420
cjwe <christajwest@gmail.com>
2025-04-12 16:08:49 +0200

    Completed module 5

--- modules/05_classes_and_inheritance.py
+++ modules/05_classes_and_inheritance.py
@@ -24,8 +24,42 @@ cat = Cat("Whiskers")
 
 dog.speak()
 cat.speak()
-
+print()
+print()
 # Practice:
 # 1. Create a class `Person` with `name` and `age` attributes
 # 2. Add a method `introduce()` that prints: "Hi, I'm [name], and I'm [age] years old."
 # 3. Create a class `Student` that inherits from `Person`, and add a `grade` attribute
+
+# 1. 
+
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age 
+
+    def introduce(self):
+        intro = f"Hi, I'm {self.name}, and I'm {self.age} years old."
+        if hasattr(self, 'grade'):
+            intro += f" I'm in the {self.grade}."
+        print(intro)
+
+person1 = Person("Christa", 30)
+person1.introduce()
+print()
+print()
+
+class Student(Person):
+    def __init__(self, name, age, grade):
+        super().__init__(name, age)
+        self.grade = grade
+
+student1 = Student("Creestah", 30, "12th Grade")
+student1.introduce()
+print()
+print()
+
+student2 = Student("Vladino", 37, "12th Grade")
+student2.introduce()
+print()
+print()
