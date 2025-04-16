70155fa6f529d48ef26c42ae9218e489c81d7038
cjwe <christajwest@gmail.com>
2025-04-16 16:02:30 +0200

    Completed module 6

--- modules/06_using_libraries.py
+++ modules/06_using_libraries.py
@@ -3,8 +3,24 @@ import datetime
 
 now = datetime.datetime.now()
 print("Current time:", now.strftime("%Y-%m-%d %H:%M:%S"))
+print()
+print()
 
 # Practice:
 # 1. Import the `random` module
 # 2. Use `random.choice()` to pick a random fruit from a list
 # 3. Use `random.randint(1, 100)` to simulate a dice roll
+
+# 1. 
+import random
+
+# 2.
+fruits = ["grape", "banana", "cherry"]
+eatMe = random.choice(fruits)
+print(f"I would love to eat a {eatMe}.")
+print()
+print()
+
+# 3.
+feelinLucky = random.randint(1,100)
+print(f"I rolled a {feelinLucky}.")
